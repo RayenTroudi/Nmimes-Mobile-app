50fa27980c4b04dbfde8f92ecfb72408c4f6352b
Rayen troudi <rayentroudi00@gmail.com>
2026-06-27 19:37:03 +0100

    feat: add locale-aware AppTextStyles.font() factory

--- lib/theme/text_styles.dart
+++ lib/theme/text_styles.dart
@@ -44,4 +44,29 @@ class AppTextStyles {
         fontWeight: FontWeight.w400,
         color: AppColors.textHint,
       );
+
+  /// Returns Poppins for EN/FR, Cairo for AR.
+  static TextStyle font(
+    BuildContext context, {
+    double fontSize = 14,
+    FontWeight fontWeight = FontWeight.w400,
+    Color? color,
+    double? height,
+  }) {
+    final isAr = Localizations.localeOf(context).languageCode == 'ar';
+    if (isAr) {
+      return GoogleFonts.cairo(
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+        color: color,
+        height: height,
+      );
+    }
+    return GoogleFonts.poppins(
+      fontSize: fontSize,
+      fontWeight: fontWeight,
+      color: color,
+      height: height,
+    );
+  }
 }
